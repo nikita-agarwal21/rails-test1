857a83c0ddf014d7de937d89f5faeb46d74225e6
nikita-agarwal21 <nikitaagarwal5092000@gmail.com>
2022-05-17 10:07:21 +0530

    added search

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,9 +1,17 @@
 module Api
-    class UsersController < ApplicationController
+   class EventsController < ApplicationController
    def typeahead
+    key=params[:input]
+    result = Event.any_of({title: /#{key}/},
 
+                          )
+    render json: result , only: [:_id ,:title, :description]
    end
-   
+   def index
+    @events = Event.all
+        
+    render json: @events
+   end
 
     end
 end
