1fca0d2557d57760e3cecbeb1336013a45ac53bd
nikita-agarwal21 <nikitaagarwal5092000@gmail.com>
2022-05-18 09:42:50 +0530

    allignment done

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,17 +1,15 @@
 module Api
    class EventsController < ApplicationController
-   def typeahead
-    key=params[:input]
-    result = Event.any_of({title: /#{key}/},
+        def typeahead
+            key=params[:input]
+            result = Event.any_of({title: /#{key}/})
+            render json: result , only: [:title, :description]
+        end
+        def index
+            @events = Event.all
+                
+            render json: @events
+        end
 
-                          )
-    render json: result , only: [:_id ,:title, :description]
-   end
-   def index
-    @events = Event.all
-        
-    render json: @events
-   end
-
-    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :api do
-  get "/events"=>"events/index"
-  get "/typeahead/:input"=>'events#typeahead'
-
+    get "/events"=>'events#index'
+    get "/typeahead/:input"=>'events#typeahead'
+  end 
 
   # Defines the root path route ("/")
   # root "articles#index"
